32e98a37ffff61192943be7a0a21622341fc7d3c
Felipe Lopes <felipelopesofc00@gmail.com>
2025-03-12 23:44:51 +0000

    versao 1.1

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -4,7 +4,7 @@ menu = '''
 [E] Extrato
 [Q] Sair
 
-'''
+=>'''
 
 saldo = 0
 limite = 500
@@ -28,7 +28,33 @@ while True:
         valor = float(input("Informe o valor do saque: "))
         excedeu_saldo = valor > saldo
         excedeu_limite = valor > limite
-        excedeu_Saques = numero_saque >= LIMTE_SAQUES
+        excedeu_saques = numero_saque >= LIMTE_SAQUES
 
         if excedeu_saldo:
             print("Operação falhou! Você não tem saldo suficiente.")
+        
+        elif excedeu_limite:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+        
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.f}\n"
+            numero_saque += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "E":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "Q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
